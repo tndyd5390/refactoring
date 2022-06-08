1a2e821db2ac76a78dec18ec3c992b45f6cd6230
waterdragon <tndyd5390@naver.com>
2022-06-09 00:09:15 +0900

    refac: decompose condition

--- src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
@@ -1,10 +1,5 @@
 package me.whiteship.refactoring._03_long_function._11_decompose_conditional;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueComment;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -12,8 +7,14 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class StudyDashboard {
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
+public class StudyDashboard {
+    // 조건문 분해하기
+    // 조건과 액션 모두 의도를 표현해야 한다.
     private final int totalNumberOfEvents;
 
     public StudyDashboard(int totalNumberOfEvents) {
@@ -61,16 +62,26 @@ public class StudyDashboard {
         new StudyPrinter(this.totalNumberOfEvents, participants).execute();
     }
 
+    //아래의 함수의 조건문을 분해해보자.
     private Participant findParticipant(String username, List<Participant> participants) {
-        Participant participant;
-        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
-            participant = new Participant(username);
-            participants.add(participant);
-        } else {
-            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-        }
+        return isNewParticipant(username, participants) ?
+                createNewParticipant(username, participants) :
+                findExistingParticipant(username, participants);
+    }
 
+    private Participant findExistingParticipant(String username, List<Participant> participants) {
+        return participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
+    }
+
+    private Participant createNewParticipant(String username, List<Participant> participants) {
+        Participant participant;
+        participant = new Participant(username);
+        participants.add(participant);
         return participant;
+    }
+
+    private boolean isNewParticipant(String username, List<Participant> participants) {
+        return participants.stream().noneMatch(p -> p.username().equals(username));
     }
 
 }
