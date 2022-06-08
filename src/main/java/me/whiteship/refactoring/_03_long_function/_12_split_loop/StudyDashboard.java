e785b9bbd1c2f27d6384a9b2e8bc60114847cd1d
waterdragon <tndyd5390@naver.com>
2022-06-09 00:27:54 +0900

    refac: split loop

--- src/main/java/me/whiteship/refactoring/_03_long_function/_12_split_loop/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_12_split_loop/StudyDashboard.java
@@ -1,10 +1,5 @@
 package me.whiteship.refactoring._03_long_function._12_split_loop;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueComment;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
@@ -14,7 +9,14 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 public class StudyDashboard {
+    // 반복문 쪼개기
+    // 성능 문제를 야기할 수 있지만 리팩토링은 성능 최적화와는 별개의 작업임, 리팩토링 이후에 성능 최적화를 시도할 수 있음
 
     private final int totalNumberOfEvents;
     private final List<Participant> participants;
@@ -22,6 +24,7 @@ public class StudyDashboard {
 
     public StudyDashboard(int totalNumberOfEvents) {
         this.totalNumberOfEvents = totalNumberOfEvents;
+        //동시에 여러 스레드에서 하나의 리스트를 수정할 경우 사용하는 리스트 컨커런시 지원
         participants = new CopyOnWriteArrayList<>();
         firstParticipantsForEachEvent = new Participant[this.totalNumberOfEvents];
     }
@@ -32,9 +35,16 @@ public class StudyDashboard {
     }
 
     private void print() throws IOException, InterruptedException {
-        GHRepository ghRepository = getGhRepository();
+        checkGithubIssues(getGhRepository());
+        new StudyPrinter(this.totalNumberOfEvents, this.participants).execute();
+        printFirstParticipants();
+    }
 
+    private void checkGithubIssues(GHRepository ghRepository) throws InterruptedException {
+        //멀티스레딩 프로그래밍 코드
+        //8개의 스레드 풀
         ExecutorService service = Executors.newFixedThreadPool(8);
+        //각 스레드가 끝날는것을 센다
         CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
 
         for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
@@ -45,20 +55,11 @@ public class StudyDashboard {
                     try {
                         GHIssue issue = ghRepository.getIssue(eventId);
                         List<GHIssueComment> comments = issue.getComments();
-                        Date firstCreatedAt = null;
-                        Participant first = null;
 
-                        for (GHIssueComment comment : comments) {
-                            Participant participant = findParticipant(comment.getUserName(), participants);
-                            participant.setHomeworkDone(eventId);
+                        //기존의 루프를 두개로 쪼갠다.
+                        checkHomework(comments, eventId);
+                        firstParticipantsForEachEvent[eventId - 1] = findFirst(comments);
 
-                            if (firstCreatedAt == null || comment.getCreatedAt().before(firstCreatedAt)) {
-                                firstCreatedAt = comment.getCreatedAt();
-                                first = participant;
-                            }
-                        }
-
-                        firstParticipantsForEachEvent[eventId - 1] = first;
                         latch.countDown();
                     } catch (IOException e) {
                         throw new IllegalArgumentException(e);
@@ -69,9 +70,26 @@ public class StudyDashboard {
 
         latch.await();
         service.shutdown();
+    }
 
-        new StudyPrinter(this.totalNumberOfEvents, this.participants).execute();
-        printFirstParticipants();
+    private Participant findFirst(List<GHIssueComment> comments) throws IOException {
+        Date firstCreatedAt = null;
+        Participant first = null;
+        for (GHIssueComment comment : comments) {
+            Participant participant = findParticipant(comment.getUserName(), participants);
+             if (firstCreatedAt == null || comment.getCreatedAt().before(firstCreatedAt)) {
+                firstCreatedAt = comment.getCreatedAt();
+                first = participant;
+            }
+        }
+        return first;
+    }
+
+    private void checkHomework(List<GHIssueComment> comments, int eventId) {
+        for (GHIssueComment comment : comments) {
+            Participant participant = findParticipant(comment.getUserName(), participants);
+            participant.setHomeworkDone(eventId);
+        }
     }
 
     private void printFirstParticipants() {
