50a582a52dea156217bcbdb0f133f1af0d47856c
waterdragon <tndyd5390@naver.com>
2022-06-08 23:10:22 +0900

    refac: preserve whole object

--- src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/StudyDashboard.java
@@ -1,22 +1,25 @@
 package me.whiteship.refactoring._03_long_function._09_preserve_whole_object;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueComment;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 public class StudyDashboard {
+
+    //객체 통째로 넘기기
+    //어떤 한 레코드에서 구할 수 있는 여러 값들을 함수에 전달하는 경우 해당 매개변수를 하나로 교체할 수 있음
+    // 이 기술을 적용하기 전에 의존성을 고려해야 한다.
 
     private final int totalNumberOfEvents;
 
@@ -78,23 +81,29 @@ public class StudyDashboard {
             writer.print(header(participants.size()));
 
             participants.forEach(p -> {
-                String markdownForHomework = getMarkdownForParticipant(p.username(), p.homework());
+                //아래 코드는 매개변수를 두개 받지만 사실 그 두개의 매개변수가 하나의 객체의 필드이다.
+                // String markdownForHomework = getMarkdownForParticipant(p.username(), p.homework());
+                String markdownForHomework = getMarkdownForParticipant(p);
                 writer.print(markdownForHomework);
             });
         }
     }
 
-    double getRate(Map<Integer, Boolean> homework) {
-        long count = homework.values().stream()
-                .filter(v -> v == true)
-                .count();
-        return (double) (count * 100 / this.totalNumberOfEvents);
-    }
+    //이 기법을 적용하기 전에 이 함수가 Participant에 의존하는게 맞는가 고민을 해봐야 한다
+    //다른 곳에서도 사용한다면 Map으로 받는게 나을수도..
+    //여기까지 고쳣다면 이 메소드의 위치는 여기가 적절한가???
+    //아래 함수는 참여율을 계산한다.
+    // double getRate(StudyDashboard studyDashboard) {
+    //     long count = homework().values().stream()
+    //             .filter(v -> v == true)
+    //             .count();
+    //     return (double) (count * 100 / studyDashboard.totalNumberOfEvents);
+    // }
 
-    private String getMarkdownForParticipant(String username, Map<Integer, Boolean> homework) {
-        return String.format("| %s %s | %.2f%% |\n", username,
-                checkMark(homework, this.totalNumberOfEvents),
-                getRate(homework));
+    private String getMarkdownForParticipant(Participant participant) {
+        return String.format("| %s %s | %.2f%% |\n", participant.username(),
+                checkMark(participant, this.totalNumberOfEvents),
+                participant.getRate(this.totalNumberOfEvents));
     }
 
     /**
@@ -118,10 +127,10 @@ public class StudyDashboard {
     /**
      * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
      */
-    private String checkMark(Map<Integer, Boolean> homework, int totalEvents) {
+    private String checkMark(Participant participant, int totalEvents) {
         StringBuilder line = new StringBuilder();
         for (int i = 1 ; i <= totalEvents ; i++) {
-            if(homework.containsKey(i) && homework.get(i)) {
+            if(participant.homework().containsKey(i) && participant.homework().get(i)) {
                 line.append("|:white_check_mark:");
             } else {
                 line.append("|:x:");
