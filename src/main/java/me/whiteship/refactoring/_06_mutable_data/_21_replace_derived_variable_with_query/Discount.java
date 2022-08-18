2c8fd9134ecb0b5bd7dfddabe0770c38e7f81f0f
park sooyong <tndyd5390@naver.com>
2022-08-18 21:33:29 +0900

    파생 변수를 질의 함수로 바꾸기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/Discount.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/Discount.java
@@ -1,8 +1,6 @@
 package me.whiteship.refactoring._06_mutable_data._21_replace_derived_variable_with_query;
 
 public class Discount {
-
-    private double discountedTotal;
     private double discount;
 
     private double baseTotal;
@@ -12,11 +10,10 @@ public class Discount {
     }
 
     public double getDiscountedTotal() {
-        return this.discountedTotal;
+        return this.baseTotal - this.discount;
     }
 
     public void setDiscount(double number) {
         this.discount = number;
-        this.discountedTotal = this.baseTotal - this.discount;
     }
 }
--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
@@ -3,17 +3,20 @@ package me.whiteship.refactoring._06_mutable_data._21_replace_derived_variable_w
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 변경할 수 있는 데이터를 최대한 줄이도록 노력해야 한다
+ * 계산해서 알아낼수 있는 변수는 제거할 수 있다.
+ * 계산에 필요한 데이터가 변하지 않는 값이라면 계산의 결과에 해당하는 데이터 역시 불변이다.
+ */
 public class ProductionPlan {
 
-    private double production;
     private List<Double> adjustments = new ArrayList<>();
 
     public void applyAdjustment(double adjustment) {
         this.adjustments.add(adjustment);
-        this.production += adjustment;
     }
 
     public double getProduction() {
-        return this.production;
+        return this.adjustments.stream().mapToDouble(Double::valueOf).sum();
     }
 }
