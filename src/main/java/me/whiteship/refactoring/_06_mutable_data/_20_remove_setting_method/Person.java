56c61b92d0cd8df0f08a36d97f739f40015db47b
park sooyong <tndyd5390@naver.com>
2022-08-18 21:17:14 +0900

    세터 제거하기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_20_remove_setting_method/Person.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_20_remove_setting_method/Person.java
@@ -1,24 +1,28 @@
 package me.whiteship.refactoring._06_mutable_data._20_remove_setting_method;
 
+/**
+ * 세터 제거하기
+ * 세터는 없애는 것이 좋다.
+ * 객체지향 체조원칙에도 나와있다.
+ */
 public class Person {
 
-    private String name;
+    private final String name;
 
-    private int id;
+    private final int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 }
--- src/test/java/me/whiteship/refactoring/_06_mutable_data/_20_remove_setting_method/PersonTest.java
+++ src/test/java/me/whiteship/refactoring/_06_mutable_data/_20_remove_setting_method/PersonTest.java
@@ -2,19 +2,15 @@ package me.whiteship.refactoring._06_mutable_data._20_remove_setting_method;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTest {
 
     @Test
     void person() {
-        Person person = new Person();
-        person.setId(10);
-        person.setName("keesun");
+        Person person = new Person("keesun", 10);
         assertEquals(10, person.getId());
         assertEquals("keesun", person.getName());
-        person.setName("whiteship");
-        assertEquals("whiteship", person.getName());
     }
 
 }
