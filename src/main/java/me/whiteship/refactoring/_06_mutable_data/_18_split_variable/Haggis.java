7cc4864940f7e2ac8f37048ff3e2ebb03b107b50
park sooyong <tndyd5390@naver.com>
2022-08-18 20:55:50 +0900

    변수 쪼개기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Haggis.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Haggis.java
@@ -1,5 +1,14 @@
 package me.whiteship.refactoring._06_mutable_data._18_split_variable;
 
+/**
+ * 변수 쪼개기
+ * 어떤 변수가 여러번 재할당 되어도 적절한 경우
+ * 반복문 순회, 값을 축적
+ * <p>
+ * 그 밖에 경우 재할당 되는 변수가 있다면 해당 변수는 여러 용도로 사용되는 것이며 변수를 분리해야 한다.
+ * 변수 하나당 하나의 책임을 지도록
+ * 상수를 사용하자(final)
+ */
 public class Haggis {
 
     private double primaryForce;
@@ -16,15 +25,15 @@ public class Haggis {
 
     public double distanceTravelled(int time) {
         double result;
-        double acc = primaryForce / mass;
+        final double primaryAcceleration = primaryForce / mass;
         int primaryTime = Math.min(time, delay);
-        result = 0.5 * acc * primaryTime * primaryTime;
+        result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
 
         int secondaryTime = time - delay;
         if (secondaryTime > 0) {
-            double primaryVelocity = acc * delay;
-            acc = (primaryForce + secondaryForce) / mass;
-            result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime + secondaryTime;
+            final double primaryVelocity = primaryAcceleration * delay;
+            final double secondaryAcceleration = (primaryForce + secondaryForce) / mass;
+            result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime + secondaryTime;
         }
 
         return result;
