9d577d0273cd612e53e4bd87ccde576b017d7cc3
park sooyong <tndyd5390@naver.com>
2022-08-24 23:00:46 +0900

    여러 함수를 변환 함수로 묶기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/Person.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/Person.java
@@ -1,5 +1,9 @@
 package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
 
+/**
+ * 어떤 객체의 변경 내역을 다른 곳으로 전파하고 싶다면 레퍼런스 객체를 사용!!
+ * 아니면 값 객체를 사용!!!
+ */
 public class Person {
 
     private TelephoneNumber officeTelephoneNumber;
@@ -9,7 +13,7 @@ public class Person {
     }
 
     public void officeAreaCode(String areaCode) {
-        this.officeTelephoneNumber.areaCode(areaCode);
+        this.officeTelephoneNumber = new TelephoneNumber(areaCode, this.officeNumber());
     }
 
     public String officeNumber() {
@@ -17,7 +21,7 @@ public class Person {
     }
 
     public void officeNumber(String number) {
-        this.officeTelephoneNumber.number(number);
+        this.officeTelephoneNumber = new TelephoneNumber(this.officeAreaCode(), number);
     }
 
 }
--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
@@ -1,24 +1,39 @@
 package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
 
+import java.util.Objects;
+
+// 현재 레퍼런스 객체
+//값 객체로 바꿔보자
 public class TelephoneNumber {
 
-    private String areaCode;
+    private final String areaCode;
 
-    private String number;
+    private final String number;
+
+    public TelephoneNumber(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
 
     public String areaCode() {
         return areaCode;
-    }
-
-    public void areaCode(String areaCode) {
-        this.areaCode = areaCode;
     }
 
     public String number() {
         return number;
     }
 
-    public void number(String number) {
-        this.number = number;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TelephoneNumber that = (TelephoneNumber) o;
+        return Objects.equals(areaCode, that.areaCode) && Objects.equals(number, that.number);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
+    }
+
 }
